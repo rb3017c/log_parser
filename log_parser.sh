97aa9e307dfe41656bf9fb17c2a5d1caec992812
rb3017c <73402430+rb3017c@users.noreply.github.com>
2020-11-18 16:24:03 -0500

    Delete log_parser.sh

--- log_parser.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-
-#Script that pulls log file from command line
-#Parse, sorts and displays unique IP Address
-
-#Usage: ./log_parser.sh (name of log_file)
-
-cat "${1}" | cut -d ' ' -f 1 | uniq | sort  -n
